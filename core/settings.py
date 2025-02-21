b3c4b0ad785d17199f2a6746595c1d81fac13f6e
enamulhaq <enamulbokshi@gmail.com>
2025-02-21 14:36:29 +0600

    Acess_token life time set to 1hr and refresh_token lifetime set to 2 days

--- core/settings.py
+++ core/settings.py
@@ -73,8 +73,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
 }
 
 ROOT_URLCONF = 'core.urls'
